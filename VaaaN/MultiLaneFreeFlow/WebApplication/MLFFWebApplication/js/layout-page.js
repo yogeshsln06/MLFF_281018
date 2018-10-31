77b3440a240323347cb30354c46da55740f852c3
vaaanmanoj <manoj.verma@vaaaninfra.com>
2018-10-31 15:38:36 +0530

    update

--- VaaaN/MultiLaneFreeFlow/WebApplication/MLFFWebApplication/js/layout-page.js
+++ VaaaN/MultiLaneFreeFlow/WebApplication/MLFFWebApplication/js/layout-page.js
@@ -47,5 +47,8 @@
             }
         });
     });
+    console.log(window.location.pathname);
+
+    $('#MainMenu [href^="' + window.location.pathname.toLowerCase() + '"]').addClass('active');
 });
 
