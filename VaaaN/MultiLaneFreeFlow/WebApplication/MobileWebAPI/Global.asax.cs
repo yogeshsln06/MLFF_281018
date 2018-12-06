8453bf24be1df4d5a8406bdc2907a182e05a7e73
Hemant Pundir <43704705+vaaanhemant@users.noreply.github.com>
2018-12-06 11:49:43 +0530

    Mobile API

--- VaaaN/MultiLaneFreeFlow/WebApplication/MobileWebAPI/Global.asax.cs
+++ VaaaN/MultiLaneFreeFlow/WebApplication/MobileWebAPI/Global.asax.cs
@@ -1,5 +1,4 @@
-﻿using MobileWebAPI.MessageHandlers;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -19,7 +18,6 @@ namespace MobileWebAPI
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            GlobalConfiguration.Configuration.MessageHandlers.Add(new ApiKeyMessageHandler());
         }
     }
 }
--- VaaaN/MultiLaneFreeFlow/WebApplication/MobileWebAPI/Properties/AssemblyInfo.cs
+++ VaaaN/MultiLaneFreeFlow/WebApplication/MobileWebAPI/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d9c3f2f8-0122-4a16-8554-56185a991043")]
+[assembly: Guid("1ffaee0b-070b-439b-88af-109c256af902")]
 
 // Version information for an assembly consists of the following four values:
 //
