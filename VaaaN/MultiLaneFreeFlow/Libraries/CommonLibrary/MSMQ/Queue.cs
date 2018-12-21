e29045e37ad4a5155bb4aefd06ce8b8f1655047f
Hemant Pundir <43704705+vaaanhemant@users.noreply.github.com>
2018-12-21 11:02:57 +0700

    update

--- VaaaN/MultiLaneFreeFlow/Libraries/CommonLibrary/MSMQ/Queue.cs
+++ VaaaN/MultiLaneFreeFlow/Libraries/CommonLibrary/MSMQ/Queue.cs
@@ -14,7 +14,7 @@ namespace VaaaN.MLFF.Libraries.CommonLibrary.MSMQ
         //public static string failedQueueNameANPR = ".\\Private$\\failedQueueANPR";
 
         public static string eventQueue = ".\\Private$\\eventQueue";
-        public static TimeSpan eventQueueTimeOut = new TimeSpan(1, 0, 3);
+        public static TimeSpan eventQueueTimeOut = new TimeSpan(0, 0, 5);
 
         public static string localEventQueue = ".\\Private$\\localEventQueue";
         public static TimeSpan localEventQueueTimeOut = new TimeSpan(1, 0, 10);
