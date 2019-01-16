0c0774eb3f279d0251ed74e54f6d4f755d81af6b
Hemant Pundir <43704705+vaaanhemant@users.noreply.github.com>
2019-01-17 01:45:38 +0700

    update

---
+++ VaaaN/MultiLaneFreeFlow/WebApplication/MLFFWebUI/Controllers/ReportController.cs
@@ -0,0 +1,45 @@
+﻿using MLFFWebUI.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VaaaN.MLFF.Libraries.CommonLibrary.BLL;
+using VaaaN.MLFF.Libraries.CommonLibrary.CBE;
+
+namespace MLFFWebUI.Controllers
+{
+    public class ReportController : Controller
+    {
+        // GET: Report
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        #region VehicleBalanceReport
+        public ActionResult VehicleBalanceReport()
+        {
+            if (Session["LoggedUserId"] == null)
+            {
+                return RedirectToAction("Logout", "Login");
+            }
+            ViewBag.MainMenu = HelperClass.NewMenu(Convert.ToInt16(Session["LoggedUserId"]), "Report", "VehicleBalanceReport");
+            ViewData["CustomerVehicle"] = CustomerVehicleBLL.GetAllAsList();
+
+
+
+            return View();
+        }
+        public string VehicleBalanceReportFilter(Int32 VehicleId, int Month, int Year)
+        {
+            DataSet ds = CustomerVehicleBLL.GetVehicleBalanceReport(VehicleId, Month, Year);
+            string Det = JsonConvert.SerializeObject(ds, Formatting.Indented);
+            return Det.Replace("\r", "").Replace("\n", "");
+        }
+
+        #endregion
+    }
+}
---
+++ VaaaN/MultiLaneFreeFlow/WebApplication/MLFFWebUI/js/Report.js
@@ -0,0 +1,31 @@
+﻿var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var month = (new Date()).getMonth() + 1;
+var Startyear = 2018;
+var year = (new Date()).getFullYear();
+var CustomerVehcileJson = [];
+function bindMonth() {
+    for (var i = 0; i < monthNames.length; i++) {
+        $("#monthList").append
+            ($('<option></option>').val(i + 1).html(monthNames[i]))
+    }
+    $("#monthList").val(month);
+}
+
+function bindYear() {
+    for (var i = Startyear; i <= year; i++) {
+        $("#yearList").append
+            ($('<option></option>').val(i).html(i))
+    }
+    $("#yearList").val(year);
+}
+
+function bindVRN() {
+    $.each((CustomerVehcileJson), function (i, vehicle) {
+        if (vehicle.VehRegNo != '') {
+            $("#vrnList").append
+            ($('<option></option>').val(vehicle.EntryId).html(vehicle.VehRegNo))
+        }
+
+    })
+}
+
