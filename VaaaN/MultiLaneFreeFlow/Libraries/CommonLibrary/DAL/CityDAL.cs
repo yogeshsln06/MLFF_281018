58e8c03467cc0044cfa8d4835a0bba9bba17bc12
mburagohain <43713825+mburagohain@users.noreply.github.com>
2019-02-12 17:52:09 +0530

    go

--- VaaaN/MultiLaneFreeFlow/Libraries/CommonLibrary/DAL/CityDAL.cs
+++ VaaaN/MultiLaneFreeFlow/Libraries/CommonLibrary/DAL/CityDAL.cs
@@ -13,6 +13,31 @@ namespace VaaaN.MLFF.Libraries.CommonLibrary.DAL
         static string tableName = "TBL_CITY";
 
         #region Get Methods
+        public static VaaaN.MLFF.Libraries.CommonLibrary.CBE.CityCBECollection GetByProvinceId(Libraries.CommonLibrary.CBE.CityCBE city)
+        {
+            VaaaN.MLFF.Libraries.CommonLibrary.CBE.CityCBECollection citys = new VaaaN.MLFF.Libraries.CommonLibrary.CBE.CityCBECollection();
+            try
+            {
+                //Stored procedure must have cur_out parameter.
+                //There is no need to add ref cursor for oracle in code.
+                string spName = VaaaN.MLFF.Libraries.CommonLibrary.Constants.oraclePackagePrefix + "CITY_GETBYPROVINCEID";
+
+                DbCommand command = VaaaN.MLFF.Libraries.CommonLibrary.DBA.DBAccessor.GetStoredProcCommand(spName);
+
+                command.Parameters.Add(VaaaN.MLFF.Libraries.CommonLibrary.DBA.DBAccessor.CreateDbParameter(ref command, "P_TMS_ID", DbType.Int32, city.TmsId, ParameterDirection.Input));
+                command.Parameters.Add(VaaaN.MLFF.Libraries.CommonLibrary.DBA.DBAccessor.CreateDbParameter(ref command, "P_PROVINCE_ID", DbType.Int32, city.ProvinceId, ParameterDirection.Input));
+                DataSet ds = VaaaN.MLFF.Libraries.CommonLibrary.DBA.DBAccessor.LoadDataSet(command, tableName);
+                DataTable dt = ds.Tables[tableName];
+                citys = ConvertDataTableToCollection(dt);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return citys;
+        }
+
         public static VaaaN.MLFF.Libraries.CommonLibrary.CBE.CityCBECollection GetAll()
         {
             VaaaN.MLFF.Libraries.CommonLibrary.CBE.CityCBECollection citys = new CBE.CityCBECollection();
@@ -36,31 +61,68 @@ namespace VaaaN.MLFF.Libraries.CommonLibrary.DAL
             return citys;
         }
 
-        public static VaaaN.MLFF.Libraries.CommonLibrary.CBE.CityCBECollection GetByProvinceId(Libraries.CommonLibrary.CBE.CityCBE city)
+        public static CBE.CityCBE GetCityById(CBE.CityCBE city)
         {
-            VaaaN.MLFF.Libraries.CommonLibrary.CBE.CityCBECollection citys = new VaaaN.MLFF.Libraries.CommonLibrary.CBE.CityCBECollection();
             try
             {
-                //Stored procedure must have cur_out parameter.
-                //There is no need to add ref cursor for oracle in code.
-                string spName = VaaaN.MLFF.Libraries.CommonLibrary.Constants.oraclePackagePrefix + "CITY_GETBYPROVINCEID";
-
+                CBE.CityCBECollection cities = new CBE.CityCBECollection();
+                string spName = Constants.oraclePackagePrefix + "CITY_GETBYID";
                 DbCommand command = VaaaN.MLFF.Libraries.CommonLibrary.DBA.DBAccessor.GetStoredProcCommand(spName);
-
-                command.Parameters.Add(VaaaN.MLFF.Libraries.CommonLibrary.DBA.DBAccessor.CreateDbParameter(ref command, "P_TMS_ID", DbType.Int32, city.TmsId, ParameterDirection.Input));
-                command.Parameters.Add(VaaaN.MLFF.Libraries.CommonLibrary.DBA.DBAccessor.CreateDbParameter(ref command, "P_PROVINCE_ID", DbType.Int32, city.ProvinceId, ParameterDirection.Input));
-                DataSet ds = VaaaN.MLFF.Libraries.CommonLibrary.DBA.DBAccessor.LoadDataSet(command, tableName);
-                DataTable dt = ds.Tables[tableName];
-                citys= ConvertDataTableToCollection(dt);
+                command.Parameters.Add(VaaaN.MLFF.Libraries.CommonLibrary.DBA.DBAccessor.CreateDbParameter(ref command, "P_CITY_ID", DbType.Int32, city.CityId, ParameterDirection.Input));
+                cities = ConvertDataTableToCollection(VaaaN.MLFF.Libraries.CommonLibrary.DBA.DBAccessor.LoadDataSet(command, tableName).Tables[tableName]);
+                return cities[0];
             }
             catch (Exception ex)
             {
                 throw ex;
             }
-
-            return citys;
         }
 
+        public static string Insert(CBE.CityCBE City)
+        {
+            try
+            {
+                string strmsg = "";
+                string spName = Constants.oraclePackagePrefix + "CITY_INSERT";
+                DbCommand command = VaaaN.MLFF.Libraries.CommonLibrary.DBA.DBAccessor.GetStoredProcCommand(spName);
+                command.Parameters.Add(VaaaN.MLFF.Libraries.CommonLibrary.DBA.DBAccessor.CreateDbParameter(ref command, "P_TMS_ID", DbType.Int32, City.TmsId, ParameterDirection.Input, 100));
+                command.Parameters.Add(VaaaN.MLFF.Libraries.CommonLibrary.DBA.DBAccessor.CreateDbParameter(ref command, "P_PROVINCE_ID", DbType.Int32, City.ProvinceId, ParameterDirection.Input, 100));
+                command.Parameters.Add(VaaaN.MLFF.Libraries.CommonLibrary.DBA.DBAccessor.CreateDbParameter(ref command, "P_CITY_NAME", DbType.String, City.CityName, ParameterDirection.Input, 100));
+                command.Parameters.Add(VaaaN.MLFF.Libraries.CommonLibrary.DBA.DBAccessor.CreateDbParameter(ref command, "P_MODIFIER_ID", DbType.Int32, City.ModifierId, ParameterDirection.Input, 100));
+                command.Parameters.Add(VaaaN.MLFF.Libraries.CommonLibrary.DBA.DBAccessor.CreateDbParameter(ref command, "P_CREATION_DATE", DbType.DateTime, City.CreationDate, ParameterDirection.Input, 100));
+                command.Parameters.Add(VaaaN.MLFF.Libraries.CommonLibrary.DBA.DBAccessor.CreateDbParameter(ref command, "P_RETURNMSG", DbType.String, "", ParameterDirection.Output, 100));
+                VaaaN.MLFF.Libraries.CommonLibrary.DBA.DBAccessor.ExecuteNonQuery(command);
+                return strmsg = (string)command.Parameters["P_RETURNMSG"].Value;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static string Update(CBE.CityCBE city)
+        {
+            try
+            {
+                string strmsg = "";
+                string spName = Constants.oraclePackagePrefix + "CITY_UPDATE";
+                DbCommand command = VaaaN.MLFF.Libraries.CommonLibrary.DBA.DBAccessor.GetStoredProcCommand(spName);
+
+                command.Parameters.Add(VaaaN.MLFF.Libraries.CommonLibrary.DBA.DBAccessor.CreateDbParameter(ref command, "P_CITY_ID", DbType.Int32, city.CityId, ParameterDirection.Input));
+                command.Parameters.Add(VaaaN.MLFF.Libraries.CommonLibrary.DBA.DBAccessor.CreateDbParameter(ref command, "P_PROVINCE_ID", DbType.Int32, city.ProvinceId, ParameterDirection.Input));
+                command.Parameters.Add(VaaaN.MLFF.Libraries.CommonLibrary.DBA.DBAccessor.CreateDbParameter(ref command, "P_CITY_NAME", DbType.String, city.CityName, ParameterDirection.Input, 100));
+                command.Parameters.Add(VaaaN.MLFF.Libraries.CommonLibrary.DBA.DBAccessor.CreateDbParameter(ref command, "P_MODIFIER_ID", DbType.Int32, city.ModifierId, ParameterDirection.Input, 100));
+                command.Parameters.Add(VaaaN.MLFF.Libraries.CommonLibrary.DBA.DBAccessor.CreateDbParameter(ref command, "P_MODIFICATION_DATE", DbType.DateTime, city.ModificationDate, ParameterDirection.Input, 100));
+                command.Parameters.Add(VaaaN.MLFF.Libraries.CommonLibrary.DBA.DBAccessor.CreateDbParameter(ref command, "P_RETURNMSG", DbType.String, "", ParameterDirection.Output, 100));
+
+                VaaaN.MLFF.Libraries.CommonLibrary.DBA.DBAccessor.ExecuteNonQuery(command);
+                return strmsg = (string)command.Parameters["P_RETURNMSG"].Value;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
         #endregion
 
         #region HelperMethods
